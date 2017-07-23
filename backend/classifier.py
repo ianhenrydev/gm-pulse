2b256e45659c4801edb1887dc7e36d330137d3da
Ian Henry <ian@ianhenry.me>
2017-07-23 17:37:57 -0400

    fixed all that

---
+++ backend/classifier.py
@@ -0,0 +1,36 @@
+from textblob.classifiers import NaiveBayesClassifier
+try:
+   import cPickle as pickle
+except:
+   import pickle
+import os
+
+def save_classifier(cl):
+    print('saving classifier')
+    f = open('output/classifier.pickle', 'wb')
+    pickle.dump(cl, f, -1)
+    f.close()
+
+def load_classifier():
+    print('loading classifier')
+    f = open('output/classifier.pickle', 'rb')
+    cl = pickle.load(f)
+    f.close()
+    return cl
+
+def test_classifier(cl):
+    with open('output/test.csv', 'r') as testFile:
+        print(str(cl.accuracy(testFile)))
+
+def get_classifier():
+    if os.path.isfile('output/classifier.pickle'):
+        cl = load_classifier()
+    else:
+        print('creating classifier')
+        with open('output/training.csv', 'r') as trainingFile:
+            cl = NaiveBayesClassifier(trainingFile, format="csv")
+            save_classifier(cl)
+    return cl
+
+cl = get_classifier()
+test_classifier(cl)
--- backend/nb.py
+++
@@ -1,4 +0,0 @@
-from textblob.classifiers import NaiveBayesClassifier
-
-with open('output/training.csv', 'r') as trainingFile:
-    cl = NaiveBayesClassifier(trainingFile, format="csv")
