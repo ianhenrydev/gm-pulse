e090a11f3d06a9a35dc9d9b529c2d6392c83f4d4
Ian Henry <ian@ianhenry.me>
2017-07-23 16:10:05 -0400

    textblob fully works

--- backend/parseJSON.py
+++ backend/parseJSON.py
@@ -1,6 +1,7 @@
 import json, csv, codecs, os
+from random import randint
 
-def writeLine(comment, out):
+def writeLine(comment, out, testOut):
 	score = int(comment['data']['controversiality'])
 	sentiment = ""
 	if score == 0:
@@ -8,32 +9,36 @@ def writeLine(comment, out):
 	else:
 		sentiment = "neg"
 	body = (comment['data']['body'].encode('ascii', 'ignore')).replace('\n', ' ').replace('\r', '')
-	out.writerow([body, sentiment])
+	if randint(0, 3) == 0:
+		testOut.writerow([body, sentiment])
+	else:
+		out.writerow([body, sentiment])
 	
 
-def getReplies(parent, out):
+def getReplies(parent, out, testOut):
 	try:
 		for comment in parent['data']['replies']['data']['children']:
-			writeLine(comment, out)
-			getReplies(comment, out)
+			writeLine(comment, out, testOut)
+			getReplies(comment, out, testOut)
 	except Exception as e:
 		pass
 	return
 
-def printThread(thread, out):
+def printThread(thread, out, testOut):
 	with open(thread) as dataFile:
 		jsonFile = json.load(dataFile)
 		thread = jsonFile[0]['data']['children'][0]['data']['name']
 		for comment in jsonFile[1]['data']['children']:
 			try:
-				writeLine(comment, out)
-				getReplies(comment, out)
+				writeLine(comment, out, testOut)
+				getReplies(comment, out, testOut)
 			except Exception as e:
 				pass
 
-directory = "soccer-data/"
+directory = "smol/"
 out = csv.writer(open("output/training.csv","wb"), delimiter=',',quoting=csv.QUOTE_ALL)
+testOut = csv.writer(open("output/test.csv","wb"), delimiter=',',quoting=csv.QUOTE_ALL)
 files = os.listdir(directory)
 for file in files:
-	printThread(directory + file, out)
+	printThread(directory + file, out, testOut)
 print('created output/training.csv')
